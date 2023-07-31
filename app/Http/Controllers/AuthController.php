41fc942d3fea706dc8c6064861ef480dba2b4aa5
ShaheerHashmi <88172667+shaheerhashmi1122@users.noreply.github.com>
2023-07-31 13:43:20 +0500

    Change login code

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,23 +11,21 @@ class AuthController extends Controller
 {
     public function login(LoginRequest $login)
     {
-        $data = $login->validated();
-        $user=User::create([
-            'name'=>$data['name'],
-            'email'=>$data['email'],
-            'password'=>bcrypt($data['password'])
-        ]);
-
-        $token=$user->createToken(name:'main')->plainTextToken;
-
-        return response(get_defined_vars());
     }
     public function signup(SigninRequest $signin)
     {
+        $data = $signin->validated();
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
 
+        $token = $user->createToken(name: 'main')->plainTextToken;
+
+        return response(get_defined_vars());
     }
     public function logout(Request $request)
     {
-
     }
 }
