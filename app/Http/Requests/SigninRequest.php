3afaea9ec4a785f2c3f2a3f2c7d8a39db2a12401
ShaheerHashmi <88172667+shaheerhashmi1122@users.noreply.github.com>
2023-07-31 14:13:08 +0500

    sign up complete

--- app/Http/Requests/SigninRequest.php
+++ app/Http/Requests/SigninRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
 
 class SigninRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class SigninRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,13 @@ class SigninRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name'=>'required|string|max:55',
+            'email'=>'required|email|unique:users,email',
+            'password'=>['required','confirmed',
+            Password::min(size:8)
+            ->letters()
+            ->symbols()
+            ]
         ];
     }
 }
