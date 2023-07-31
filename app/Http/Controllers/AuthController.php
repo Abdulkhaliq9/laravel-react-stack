63e05e7b9b2edabea4e4336e9a31202cd138d62a
ShaheerHashmi <88172667+shaheerhashmi1122@users.noreply.github.com>
2023-07-31 13:56:10 +0500

    auth done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,11 +6,23 @@ use App\Http\Requests\LoginRequest;
 use App\Http\Requests\SigninRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function login(LoginRequest $login)
     {
+        $credentials = $login->validated();
+        if (!Auth::attempt($credentials)) {
+            return response([
+                'message' => 'Provided Email address or password is incorrect'
+            ]);
+
+            $user = Auth::user();
+            $token = $user->createToken(name: 'main')->plainTextToken;
+
+            return response(get_defined_vars());
+        }
     }
     public function signup(SigninRequest $signin)
     {
@@ -27,5 +39,7 @@ class AuthController extends Controller
     }
     public function logout(Request $request)
     {
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
     }
 }
