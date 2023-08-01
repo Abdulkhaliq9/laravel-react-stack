c9166a62b699fe73c07c550d38167f9bb346f233
Abdulkhaliq9 <108611214+Abdulkhaliq9@users.noreply.github.com>
2023-08-01 13:17:42 +0500

    axios maintaines

--- react/src/views/SignUp.jsx
+++ react/src/views/SignUp.jsx
@@ -1,19 +1,36 @@
 import React from "react";
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
 export default function SignUp() {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const confirmpasswordRef = useRef();
+
   // form validation
   const onSubmit = (ev) => {
-    ev.preventDefault;
+    ev.preventDefault();
+    const payload = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      confirm_password: confirmpasswordRef.current.value,
+    };
+    console.log(payload)
   };
   return (
     <>
       <h1 className="title">SignUp for free</h1>
       <form onSubmit={onSubmit}>
-        <input type="text" placeholder="Full Name" />
-        <input type="email" placeholder="Email Address" />
-        <input type="password" placeholder="Password" />
-        <input type="password" placeholder="Confirm Password" />
+        <input ref={nameRef} type="text" placeholder="Full Name" />
+        <input ref={emailRef} type="email" placeholder="Email Address" />
+        <input ref={passwordRef} type="password" placeholder="Password" />
+        <input
+          ref={confirmpasswordRef}
+          type="password"
+          placeholder="Confirm Password"
+        />
         <button className="btn btn-block">SignUp</button>
         <p className="message">
           Already Registered? <Link to="/login">Sign in</Link>
